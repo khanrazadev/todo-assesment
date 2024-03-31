3eb96c46cdc2a2393e1f5fdc9ecb84c4c54f4574
raza <razakhanha05@gmail.com>
2024-03-31 22:02:08 +0530

    Added poppins fonts -rm default page

---
+++ app/(todos)/layout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodosLayout = ({ children }) => {
+  return <main className="bg-red-500 h-full">{children}</main>;
+};
+
+export default TodosLayout;
---
+++ app/(todos)/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TodosPage = () => {
+  return (
+    <div className="">
+      {/* Add task */}
+      <div>
+        <label className="text-headline">Add task</label>
+      </div>
+    </div>
+  );
+};
+
+export default TodosPage;
