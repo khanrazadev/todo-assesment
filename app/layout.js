d3fbe14302a392c6c652225df0dd14ccd9889356
raza <razakhanha05@gmail.com>
2024-04-01 06:18:21 +0530

    Setup redux store

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Providers from "@/lib/features/Provider";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500"] });
 
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
